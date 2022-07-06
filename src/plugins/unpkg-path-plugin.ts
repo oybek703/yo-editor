be296aa07de1ed0cd84c945caa184290d5988fcb
Yoriqulov Oybek <hhoybek@gmail.com>
2022-07-06 12:02:24 +0500

    caching implemented using indexedDB

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,8 @@
 import * as esbuild from 'esbuild-wasm'
 import axios from 'axios'
+import localForage from 'localforage'
+
+const fileCache = localForage.createInstance({name: 'fileCache'})
 
 export const unpkgPathPlugin = () => {
   return {
@@ -35,12 +38,16 @@ export const unpkgPathPlugin = () => {
             `,
           }
         }
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path)
+        if(cachedResult) return cachedResult
         const {data, request} = await axios.get(args.path)
-        return {
+        const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents: data,
           resolveDir: new URL('./', request.responseURL).pathname,
         }
+        await fileCache.setItem(args.path, result)
+        return result
       })
     },
   }
